f086a2b0d776bd9a7531d57e3062ab3335df0e69
ericawu <erica15wu@gmail.com>
2016-08-22 09:46:34 -0700

    lists all user emails

--- client/main.js
+++ client/main.js
@@ -4,11 +4,15 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 PlayersList = new Mongo.Collection('playersList');
+Meteor.subscribe('topUsers');
 
 Template.main.helpers({
 	topPlayers: function() {
-		console.log("test");
+		//Meteor.subscribe('allEmails');
+		
 		return Meteor.users.find();
 	}
-})
+});
+
+
 
--- server/main.js
+++ server/main.js
@@ -2,4 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.publish('topUsers', function() {
+  	return Meteor.users.find({}, {fields: {emails: 1}});
+  });
 });
